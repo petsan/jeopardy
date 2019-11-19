6e42c837f58059352a6817b80fc741d13b102b18
laura antonius <laurastolen@gmail.com>
2019-11-19 12:59:41 -0800

    added event listener for clue div

--- scripts/app.js
+++ scripts/app.js
@@ -1,70 +1,5 @@
 'use strict';
 
-// Debugging shortcuts
-// To address data at specific locations do this:
-// console.log('the value at bottom right is: ' + jeopardyBoard[0].questions[4][0]);
-// console.log('clue at top left: ' + jeopardyBoard[0].questions[0][1]);
-// console.log('answer at bottom right: ' + jeopardyBoard[5].questions[4][2]);
-// console.log('logs out the entire array with all the objects: ' + jeopardyBoard);
-
-// this is an array that holds all category objects
-var jeopardyBoard = [];
-
-// this function creates a Category object and pushes it to jeopardyBoard
-function Category(name, questions) {
-  this.name = name;
-  this.questions = questions;
-  jeopardyBoard.push(this);
-  //the above line takes this object and adds it to that, this is why i don't have list them out
-}
-
-// structure [0: points value, clue, answer, isShownFalg]
-var cat0 = new Category('science', [
-  [100, 'It\'s the largest blood vessel in the body', 'The Aorta?', true],
-  [200, 'Earth is farthest from the sun during this month', 'July', true],
-  [300, 'This hardest tissue in the body covers the crown of a tooth', 'Enamel', true],
-  [400, '18399 feet down the Molloy Deep is the deepest point in this ocean', 'The Arctic', false],
-  [500, 'This element\'s symbol W comes from wolfram its alternate name', 'Tungsten', true]
-]);
-
-var cat1 = new Category('war stories', [
-  [100, 'The African Queen by C.S. Forester', 'world war I', true],
-  [200, 'Mister Roberts & The Naked and the Dead', 'world war II', true],
-  [300, 'Gods and Generals by Jeff Shaara', 'the civil war', true],
-  [400, 'Rise to Rebellion by Jeff Shaara', 'the american revolution', true],
-  [500, 'The Last of the Mohicans', 'The French and Indian wars', true]
-]);
-
-var cat2 = new Category('Before & After', [
-  [100, 'clue', 'question', true],
-  [200, 'clue', 'question', true],
-  [300, 'clue', 'question', true],
-  [400, 'clue', 'question', true],
-  [500, 'clue', 'question', true]]);
-
-var cat3 = new Category('American History', [
-  [100, 'clue', 'question', true],
-  [200, 'clue', 'question', true],
-  [300, 'clue', 'question', true],
-  [400, 'clue', 'question', true],
-  [500, 'clue', 'question', true]]);
-
-var cat4 = new Category('Word Origins', [
-  [100, 'clue', 'question', true],
-  [200, 'clue', 'question', true],
-  [300, 'clue', 'question', true],
-  [400, 'clue', 'question', true],
-  [500, 'clue', 'question', true]]);
-
-var cat5 = new Category('category 5', [
-  [100, 'clue', 'question', true],
-  [200, 'clue', 'question', true],
-  [300, 'clue', 'question', true],
-  [400, 'clue', 'question', true],
-  [500, 'clue', 'question at bottom right', true]]);
-
-///////////////// Peter: I do not believe I changed any of yesterday's code between these comments
-
 var category = [
   'Before & After',
   'Science',
@@ -114,8 +49,6 @@ function renderHeader(domReference) {
 
   for (var i = 0; i < category.length; i++) {
     var td = document.createElement('td');
-    td.setAttribute('class', 'card');
-    td.setAttribute('id', 'cat' + i);
     td.textContent = category[i];
     tr.append(td);
   }
@@ -173,7 +106,7 @@ function findObject(arr) {
   // figure out which pointValue array
   var arrayFinder = `array${arr[0]}`;
   arrayFinder = window[arrayFinder];
-  console.log(arrayFinder);
+
   // figure out which object in the array we want
   var cellFinder = arrayFinder[arr[1]];
   return cellFinder;
@@ -183,68 +116,30 @@ function tdClickManager(event) {
   // call id splitter and object finder functions when a cell is clicked on, to get the object
   var idReference = idSplitter(event.target.id);
   var objectReference = findObject(idReference);
-  console.log(objectReference.clue);
   table.innerHTML = '';
+
   var clueDisplayDiv = document.createElement('div');
   clueDisplayDiv.textContent = objectReference.clue;
+  clueDisplayDiv.setAttribute('id', event.target.id);
+
   table.append(clueDisplayDiv); // accessed the clue and put it on the screen
   clueDisplayDiv.addEventListener('click', clueClickManager);
 }
 
+
 function clueClickManager(event) {
   table.innerHTML = '';
-  // stopped here. next step is to display the question and buttons to indicate team correct/incorrect, which changes scores
+  var questionDisplayDiv = document.createElement('div');
+
+  var idReference = idSplitter(event.target.id);
+  var objectReference = findObject(idReference);
+
+  questionDisplayDiv.textContent = objectReference.question;
+  table.append(questionDisplayDiv);
+  // stopped here. next step is to display the buttons to indicate team correct/incorrect, which changes scores
 }
+
+
 
 renderHeader(table);
 renderBody(table);
-
-////////////////////// Peter above this line is yesterday's code
-
-function renderBoard(domReference) {
-  var tr1 = document.createElement('tr');
-
-  //render top row
-  for ( var columnIndexTopRow = 0; columnIndexTopRow < jeopardyBoard.length; columnIndexTopRow++ )
-  {
-    var td1 = document.createElement('td');
-    td1.setAttribute('class', 'card');
-    td1.setAttribute('id', 'category' + columnIndexTopRow);
-    td1.addEventListener('click', tdClickManager);
-    td1.textContent = jeopardyBoard[columnIndexTopRow].name;
-    tr1.append(td1);
-  }
-  domReference.append(tr1);
-
-  //render the other rows
-  for ( var rowIndex = 0; rowIndex < 5; rowIndex++ ) {
-    var tr2 = document.createElement('tr');
-
-    for ( var columnIndexBody = 0; columnIndexBody < 6; columnIndexBody++ ) {
-      var currentValue = jeopardyBoard[columnIndexBody].questions[rowIndex][0];
-      var isShownFlag = jeopardyBoard[columnIndexBody].questions[rowIndex][3];
-
-      var td2 = document.createElement('td');
-      var ahref = document.createElement('a');
-
-      if (isShownFlag === true) {
-        td2.setAttribute('class', 'card');
-        td2.setAttribute('id', 'category' + columnIndexBody + 'clue' + rowIndex);
-
-        ahref.setAttribute('href', 'clue.html');
-        ahref.textContent = currentValue;
-        ahref.addEventListener('click', tdClickManager);
-
-        td2.append(ahref);
-      } else {
-        td2.append(ahref.textContent = '');
-      }
-
-      tr2.append(td2);
-    }
-    domReference.append(tr2);
-  }
-}
-
-var jeopardyDOM = document.getElementById('jeopardy-board');
-renderBoard(jeopardyDOM);
